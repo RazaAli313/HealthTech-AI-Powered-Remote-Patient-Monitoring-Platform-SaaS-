83fb9fb49fcc3806d68ce66c3e6d0a0e7c77993d
RazaAli313 <razaalipk313@gmail.com>
2025-07-22 19:20:05 +0500

    Added Public Routes(Home,About,Contact),further refined project structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
-
-
+import Navbar from './components/layouts/Navbar.tsx'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/public/Home.tsx'
+import About from './pages/public/About.tsx'
+import Contact from './pages/public/Contact.tsx'
 function App() {
 
   return (
     <>
-      <h1 className='bg bg-blue-500'>Health Tech</h1>
+    <Navbar></Navbar>
+    <main>
+    <div className='container bg-blue-400'
+    >Health Tech is an AI powered SAAS based platform which is used for remote patient management... </div>
+     <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+     </Routes>
+    </main>
+    <img></img>
+    {/* <Footer></Footer> */}
+
     </>
   )
 }
--- frontend/src/components/layouts/Navbar.tsx
+++ frontend/src/components/layouts/Navbar.tsx
@@ -1,9 +1,16 @@
-
-
+import {Link} from 'react-router-dom'
 export default function Navbar() {
   return (
-    <div>
-   
-    </div>
+    <nav className="  mt-0 pt-2 space-x-3 text-center">
+
+     <Link to="/">Home</Link>
+     <Link to="/about">About</Link>
+     <Link to="/contact">Contact us</Link>
+     
+    
+
+    
+    </nav>
+
   )
 }
